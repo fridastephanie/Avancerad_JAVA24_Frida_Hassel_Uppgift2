90e412359da744afb4ff3fede789f2179ebbc1d9
Frida Hassel <frida.hassel@live.se>
2024-12-11 08:11:35 +0100

    Byter namn på package till main, skapar anrop i klassen Main och skapar textfilen students.txt

---
+++ Avancerad_JAVA24_Frida_Hassel_Uppgift2/src/main/java/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+import ui.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        //Skapar ett objekt av klassen Menu, som skriver ut menyn
+        Menu menu = new Menu();
+    }
+}
